5cf921d15483f2c89d9bdc67e474223b99a376af
Aleksey Unzon <128930170+A1eKs94@users.noreply.github.com>
2026-07-15 16:31:37 -0700

    Revert "Update verifyBearerToken.js"
    This reverts commit b79e867f0b62ca4b8a7d61bcc7e5559c4db67db6.

--- src/middlewares/verifyBearerToken.js
+++ src/middlewares/verifyBearerToken.js
@@ -1,23 +1,17 @@
 const verifyBearerToken = (req, res, next) => {
-  if (req.method === "OPTIONS") {
-    return next();
-  }
-
-  const authHeader =
-    req.headers["authorization"] || req.headers["Authorization"];
-  const tokenCliente = authHeader && authHeader.split(" ")[1];
-
-  const tokenCorrecto =
-    process.env.APP_BEARER_TOKEN || process.env.BEARER_TOKEN;
+  const authHeader = req.headers['authorization'];
+  
+  const tokenCliente = authHeader && authHeader.split(' ')[1];
+  const tokenCorrecto = process.env.APP_BEARER_TOKEN;
 
   if (!tokenCliente || tokenCliente !== tokenCorrecto) {
-    return res.status(401).json({
-      ok: false,
-      message: "Acceso denegado. Bearer token inválido o ausente.",
+    return res.status(401).json({ 
+      ok: false, 
+      message: 'Acceso denegado. Bearer token inválido o ausente.' 
     });
   }
 
-  next();
+  next(); 
 };
 
 module.exports = verifyBearerToken;
