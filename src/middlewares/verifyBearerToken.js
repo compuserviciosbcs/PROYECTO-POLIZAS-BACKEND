b79e867f0b62ca4b8a7d61bcc7e5559c4db67db6
Aleksey Unzon <128930170+A1eKs94@users.noreply.github.com>
2026-07-15 15:19:55 -0700

    Update verifyBearerToken.js

--- src/middlewares/verifyBearerToken.js
+++ src/middlewares/verifyBearerToken.js
@@ -1,17 +1,23 @@
 const verifyBearerToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  
-  const tokenCliente = authHeader && authHeader.split(' ')[1];
-  const tokenCorrecto = process.env.APP_BEARER_TOKEN;
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+
+  const authHeader =
+    req.headers["authorization"] || req.headers["Authorization"];
+  const tokenCliente = authHeader && authHeader.split(" ")[1];
+
+  const tokenCorrecto =
+    process.env.APP_BEARER_TOKEN || process.env.BEARER_TOKEN;
 
   if (!tokenCliente || tokenCliente !== tokenCorrecto) {
-    return res.status(401).json({ 
-      ok: false, 
-      message: 'Acceso denegado. Bearer token inválido o ausente.' 
+    return res.status(401).json({
+      ok: false,
+      message: "Acceso denegado. Bearer token inválido o ausente.",
     });
   }
 
-  next(); 
+  next();
 };
 
 module.exports = verifyBearerToken;
